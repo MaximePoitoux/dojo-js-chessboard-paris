1ddafea4c6c0a123f231941c8174bc48fc683f8b
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-04-01 10:44:35 +0200

    Co-authored-by: AuxB <49131448+AuxB@users.noreply.github.com> Co-authored-by: mohamedabidi1105 <mohamed.abidi1105@gmail.com>

--- chessboard.js
+++ chessboard.js
@@ -1,23 +1,22 @@
 const chessboard = () => {
-
-    const tabNb = [1, 2, 3, 4, 5, 6, 7, 8]
-    const tab = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    const tabNb = [1,2,3,4,5,6,7,8]
+    const tab = ["a","b","c","d","e","f","g","h"]
     const resultatTab = []
     let i = 0
-    
+    let j = 0
 
-    while (i < tab.length) {
-        let j = 0
-        while (j < tabNb.length) {
-            resultatTab.push(`${tab[i]} - ${tabNb[j]}`)
-            j++
-        }
-        i++
-    }
-    return resultatTab
+    while(i < tab.length) {
+        while(j < tabNb.length) {
+        resultatTab.push(`${tab[i]} - ${tabNb[j]}`)
+        j++
+}
+j=0
+i++
+}
+console.log(resultatTab)
 }
 
-console.log(chessboard())
+
 
 
 module.exports = chessboard
@@ -29,26 +28,3 @@ module.exports = chessboard
 
 
 
-
-
-// const chessboard = () => {
-//     const tabNb = [1,2,3,4,5,6,7,8]
-//     const tab = ["a","b","c","d","e","f","g","h"]
-//     const resultatTab = []
-//     let i = 0
-//     let j = 0
-
-//     while(i < tab.length) {
-//         while(j < tabNb.length) {
-//         resultatTab.push(`${tab[i]} - ${tabNb[j]}`)
-//         j++
-// }
-// j=0
-// i++
-// }
-// console.log(resultatTab)
-// }
-
-
-
-// module.exports=chessboard
