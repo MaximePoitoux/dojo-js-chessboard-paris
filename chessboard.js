226e4c1bb63f806da07cf39c448adb615a40178a
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-04-01 11:22:58 +0200

    final

--- chessboard.js
+++ chessboard.js
@@ -1,22 +1,16 @@
+
 const chessboard = () => {
-    const tabNb = [1,2,3,4,5,6,7,8]
-    const tab = ["a","b","c","d","e","f","g","h"]
+    const tabNb = [1, 2, 3, 4, 5, 6, 7, 8]
+    const tabLetters = ["a", "b", "c", "d", "e", "f", "g", "h"]
     const resultatTab = []
-    let i = 0
-    let j = 0
+// [[1][2][3][4][5]]
+    //letter = tabLetters[i]
+    for(let letter of tabLetters){
+        resultatTab.push(tabNb.map(number => `${letter} - ${number}`))
+    }
 
-    while(i < tab.length) {
-        while(j < tabNb.length) {
-        resultatTab.push(`${tab[i]} - ${tabNb[j]}`)
-        j++
+    return resultatTab
 }
-j=0
-i++
-}
-console.log(resultatTab)
-}
-
-
 
 
 module.exports = chessboard
@@ -24,7 +18,24 @@ module.exports = chessboard
 
 
 
+// const chessboard = () => {
+//     const tabNb = [1, 2, 3, 4, 5, 6, 7, 8]
+//     const tab = ["a", "b", "c", "d", "e", "f", "g", "h"]
+//     const resultatTab = []
+//     let i = 0
 
 
-
+//     while (i < tab.length) {
+//         let j = 0
+//         const colonneTab = []
+//         while (j < tabNb.length) {
+//             //["a - 1", "a - 2", "a - 3", "a - 4", "a - 5", "a - 6", "a - 7", "a - 8"]
+//             colonneTab.push(`${tab[i]} - ${tabNb[j]}`)
+//             j++
+//         }
+//         resultatTab.push(colonneTab)
+//         i++
+//     }
+//     return resultatTab
+// }
 
